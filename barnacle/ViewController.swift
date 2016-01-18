9514635a9c44148dc9a2539ad5c9558be3cb4dc1
chris <chume@gmail.com>
2016-01-17 22:22:19 -0600

    Initial Commit

---
+++ barnacle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  barnacle
+//
+//  Created by Chris Hume on 1/17/16.
+//  Copyright © 2016 Chris Hume. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
